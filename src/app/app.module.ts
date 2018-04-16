cee1f718e18807326695b2fa713705ec3979aac8
joexfactor <joexfactor@gmail.com>
2018-04-16 13:02:43 +1000

    initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NewsComponent } from './components/news/news.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    OfferComponent,
+    MenuComponent,
+    NewsComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
