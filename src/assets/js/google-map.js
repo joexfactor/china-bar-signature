9a884021982f773aedfe58543949fb59b08dbaa6
junweipan <panjunweide@gmail.com>
2018-04-17 15:45:24 +1000

    move reservation data-target to home page

--- src/assets/js/google-map.js
+++ src/assets/js/google-map.js
@@ -5,18 +5,19 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    var myLatlng = new google.maps.LatLng(-37.855180, 145.168103);
+// china bar lat/lng
+  //-37.855180
+  //145.168103
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         scrollwheel: false,
         styles: [
             {
@@ -34,16 +35,16 @@ function init() {
         ]
     };
 
-    
 
-    // Get the HTML DOM element that will contain your map 
+
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
+
+    var addresses = ['China Bar Signature Burwood'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
@@ -52,11 +53,11 @@ function init() {
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                icon: 'assets/images/myloc.png'
             });
 
         });
     }
-    
+
 }
 google.maps.event.addDomListener(window, 'load', init);
