4936ff7863b939f9e30b35829490a7e526bf0d03
joexfactor <joexfactor@gmail.com>
2018-04-21 14:56:16 +1000

    test

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Install express server
+// //Install express server
 const express = require('express');
 const app = express();
 
@@ -7,3 +7,5 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
