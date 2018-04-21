648370df880d5b6371b267e3b9a171706d3464d7
joexfactor <joexfactor@gmail.com>
2018-04-21 15:04:30 +1000

    test

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-// //Install express server
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-// Serve only the static files form the dist directory
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
