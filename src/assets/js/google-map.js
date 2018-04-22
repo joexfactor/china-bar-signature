dfcbe0b9f804e373d85e4d4a256105a22729262c
junweipan <panjunweide@gmail.com>
2018-04-22 22:48:35 +1000

    modify reservation

--- src/assets/js/google-map.js
+++ src/assets/js/google-map.js
@@ -44,20 +44,19 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    var addresses = ['China Bar Signature Burwood'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'assets/images/myloc.png'
-            });
-
-        });
-    }
+    // var addresses = ['China Bar'];
+    // for (var x = 0; x < addresses.length; x++) {
+    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+    //         var p = data.results[0].geometry.location
+    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
+    //         new google.maps.Marker({
+    //             position: latlng,
+    //             map: map,
+    //             icon: 'assets/images/myloc.png'
+    //         });
+    //
+    //     });
+    // }
 
 }
 google.maps.event.addDomListener(window, 'load', init);
